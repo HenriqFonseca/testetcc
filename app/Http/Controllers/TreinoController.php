d75a8754d0f013c0a45c9deb96a73f84ad16d30f
Henriq_Fonseca <henrique12095@gmail.com>
2022-10-23 18:33:58 -0300

    Criei a estrutura q vai ser utilizada

--- app/Http/Controllers/TreinoController.php
+++ app/Http/Controllers/TreinoController.php
@@ -2,31 +2,86 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Models\Treino;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TreinoController extends Controller
 {
-    public function aluno(){
-        return view('treino.aluno');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $treinos = Treino::all();
+        return view('treino.professor' , ['treinos' => $treinos]);
     }
 
-    public function professor(){
-        return view('treino.professor');
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    public function show(){
-        return view('');
-    }
-    public function create(){
-        
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function edit(){
-        
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
     }
 
-    public function delete(){
-        
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
