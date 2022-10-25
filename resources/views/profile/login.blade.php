f234e425ff9f7054c6ca4b785fb009eb41463d3d
Henriq_Fonseca <henrique12095@gmnail.com>
2022-10-24 21:46:37 -0300

    a

--- resources/views/profile/login.blade.php
+++ resources/views/profile/login.blade.php
@@ -12,28 +12,39 @@
 </head>
 
 <body>
+
+    <style>
+        .new-container{
+            display: flex;
+            flex-direction: column;
+            width: 430px;
+        }
+    </style>
+    <div class="new-container">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+
+            <p>{{ $error }}</p>
+            @endforeach
+        </div>
+        @endif
         <div class="wrapper">
             <div class="cadastro">
-                
-                @if ($errors->any())
-                  <div class="alert alert-danger">
-                          @foreach ($errors->all() as $error)
-                              <p>{{ $error }}</p>
-                          @endforeach
-                  </div>
-              @endif
+
+
                 <form class="form-signin" method="POST" action="{{ route('auth2') }}">
-                  @csrf
+                    @csrf
                     <h2 class="form-signin-heading">Insira seu cadastro</h2>
                     <br>
                     <input type="text" class="input" name="cpf" placeholder="CPF" autofocus="" />
                     <br>
                     <input type="password" class="input" name="password" placeholder="Senha" />
-                        <br>
-                        <button class="login-button">
-                            Fazer Login
-                        </button><br>
-                       
+                    <br>
+                    <button class="login-button">
+                        Fazer Login
+                    </button><br>
+
                 </form>
                 <ul class="nav-item-box">
                     <a class="forgot-password" href="">
@@ -43,16 +54,17 @@
             </div>
             <a href="{{ route('home.index') }}"class="logo-box">
                 <img class="img" src="img/logo-vertical.png" alt="">
-                <a>
+            </a>
+                </div>
+                <div class="drops">
+                    <div class="drop drop-1"></div>
+                    <div class="drop drop-2"></div>
+                    <div class="drop drop-3"></div>
+                    <div class="drop drop-4"></div>
+                    <div class="drop drop-5"></div>
+                </div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         </div>
-        <div class="drops">
-            <div class="drop drop-1"></div>
-            <div class="drop drop-2"></div>
-            <div class="drop drop-3"></div>
-            <div class="drop drop-4"></div>
-            <div class="drop drop-5"></div>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 
-</body>
+        </body>
